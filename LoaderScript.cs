71c66dac3843edb0175adb9a6a75dc02ad8c4720
LilankaUdawatta <39363703+LilankaUdawatta@users.noreply.github.com>
2018-07-24 20:08:21 +0530

    Add files via upload

---
+++ LoaderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderScript : MonoBehaviour {
+	//Loading effect
+	public bool loading = false;
+	public Texture loadingTexture;
+	public float size = 250.0f;
+	private float rotAngle = 0.0f;
+	public float rotSpeed = 1.0f;
+	private float angles = 0.0f;//
+
+	void  Update (){
+		if(loading){
+			rotAngle += rotSpeed * Time.deltaTime;
+		}	
+	}
+
+	void  OnGUI (){
+		if(loading){
+			Vector2 pivot = new Vector2(Screen.width/2, Screen.height/2);
+			GUIUtility.RotateAroundPivot(rotAngle%360,pivot);
+			GUI.DrawTexture( new Rect((Screen.width - size)/2 , (Screen.height - size)/2, size, size), loadingTexture);
+		
+		}
+	}
+}
---
+++ SignOutScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SignOutScript : MonoBehaviour {
+
+	public void start()
+	{
+		SceneManager.LoadScene("Landing Page");
+	}
+}
